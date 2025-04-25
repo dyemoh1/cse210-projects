1b56248679cb4b2e776cc97c6dc2f3e97e01b380
dyemoh1 <nii23002@byui.edu>
2025-04-24 18:17:55 -0600

    I added the assigment prompts to Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("what is your first name? ");
+        string first_name = Console.ReadLine(); 
+        Console.Write("what is your Last name? ");
+        string Last_name = Console.ReadLine(); 
+
+        Console.WriteLine($"Your name is {Last_name}, {first_name} {Last_name}. ");
     }
 }
