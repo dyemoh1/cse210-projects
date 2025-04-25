38a2b9f170b4df20164129333c868c2a8e38b122
dyemoh1 <nii23002@byui.edu>
2025-04-25 12:57:16 -0600

    grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,51 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+   Console.Write("Please input your grade ");
+    string grade = Console.ReadLine();
+    int number = int.Parse(grade);
+    string letter ="";
+
+    if (number >= 90)
+{
+    letter="A";
+    
+}
+else if (number >= 80)
+{
+    letter="B";
+    
+}
+else if (number >= 70 )
+{
+    letter="C";
+    
+}
+else if (number>= 60 )
+{
+    letter="D";
+    
+}
+else 
+{
+    letter="F";
+    
+}
+Console.WriteLine($"Your grade is: {letter}");
+if (number > 70)
+{
+Console.WriteLine("Congratulations! You have passed this test");
+}
+else
+{
+Console.WriteLine("Sorry, You can take the test again.");
+}
+
+
+
+
+
+
     }
 }
